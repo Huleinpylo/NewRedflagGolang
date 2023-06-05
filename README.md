dab892ca5e3a7b12a0be6ac4cc4769f8b43caa15
lil-doudou <67382584+lil-doudou@users.noreply.github.com>
2023-06-05 10:13:58 +0200

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # NewRedflag
 Basic Python script for download/create wordlist from https://red.flag.domains/
+
+## Install
+```python3
+git clone https://github.com/lil-doudou/NewRedflag.git
+cd NewRedFlag
+pip3 install -r requirements.txt
+```
+
+## Usage
+```
+python3 newflag.py
+```
+```
+usage: newflag.py [-h] [--latest] [--day DAY] [--all]
+
+options:
+  -h, --help         show this help message and exit
+  --latest, -l       latest list
+  --day DAY, -d DAY  Day of list
+  --all, -A          All list
+```
+
+## Support
+https://www.buymeacoffee.com/0xlildoudou
---
+++ newflag.py
@@ -0,0 +1,59 @@
+import requests, datetime, argparse
+from bs4 import BeautifulSoup
+
+def download_all(link):
+    new_redflagdomain = requests.get('https://dl.red.flag.domains/daily/' + link.get('href')).text
+    filename = "redflag_all.txt"
+    file = open(filename, "a")
+    file.write(new_redflagdomain)
+    file.close()
+
+def download_date(date, link):
+    new_redflagdomain = requests.get('https://dl.red.flag.domains/daily/' + link.get('href')).text
+    filename = "redflag_" + str(date) + ".txt"
+    file = open(filename, "a")
+    file.write(new_redflagdomain)
+    file.close()
+
+def main(latest, day, all):
+    if not day or latest == True:
+        date = datetime.datetime.now() - datetime.timedelta(days=1)
+        yesterday_date = date.strftime("%Y-%m-%d")
+    else:
+        date = datetime.datetime.now() - datetime.timedelta(days=int(day))
+        specific_date = date.strftime("%Y-%m-%d")
+    main_url = requests.get('https://dl.red.flag.domains/daily/').text
+    main_page = BeautifulSoup(main_url, 'html.parser')
+    for link in main_page.find_all('a'):
+        date_url = link.get('href')
+        if date_url[:4].isdigit():
+            try:
+                if (yesterday_date in date_url) and (latest == True):
+                    print("Download : " + str(date_url))
+                    download_date(yesterday_date, link)
+            except:
+                pass
+
+            try:
+                if (specific_date in date_url) and day:
+                    print("Download : " + str(date_url))
+                    download_date(specific_date, link)
+            except:
+                pass
+
+            if all == True:
+                print("Download : " + str(date_url))
+                download_all(link)
+            
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--latest","-l", help="latest list", action="store_true")
+    parser.add_argument("--day","-d", help="Day of list")
+    parser.add_argument("--all","-A", help="All list", action="store_true")
+    args = parser.parse_args()
+
+    latest = args.latest
+    day = args.day
+    all = args.all
+    main(latest, day, all)
